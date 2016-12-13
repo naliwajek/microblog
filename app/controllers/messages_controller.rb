0ba11d5eab4749fd87a528cf0559663f51c864df
Jakub Naliwajek <jakub.naliwajek@airhelp.com>
2016-12-13 12:44:00 +0100

    Add basic form for sending messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,27 @@
 class MessagesController < ApplicationController
   def create
+    message = current_user.messages.build(message_params)
+
+    if message.save
+      flash[:success] = 'Thanks! Messages was posted to your feed!'
+    end
+
+    redirect_to root_url
   end
 
   def destroy
   end
 
   def index
-    render :index, 
-      locals: { messages: current_user.messages.desc }
+    render :index, locals: {
+      messages: current_user.messages.desc,
+      new_message: current_user.messages.build
+    }
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
