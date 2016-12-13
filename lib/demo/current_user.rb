1a2c01218c6ec71af383e9555c29c3de9f12a3fc
Jakub Naliwajek <jakub.naliwajek@airhelp.com>
2016-12-13 10:41:15 +0100

    Add demo version of `current_user` method

---
+++ lib/demo/current_user.rb
@@ -0,0 +1,15 @@
+# This module keeps logic required to present a demo
+# of a timeline. It should be replaced later on with
+# real methods.
+
+module Demo
+  module CurrentUser
+
+    # Because demo does not require aunthentication,
+    # the first user is used everywhere as a currently
+    # logged in user - to be replacted by Devise#current_user
+    def current_user
+      User.first
+    end
+  end
+end
